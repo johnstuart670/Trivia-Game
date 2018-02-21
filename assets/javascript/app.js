571b1269e9f8cbaf02d9826e54d6a66267d1337a
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-02-20 21:56:22 -0700

    timeout function now working in javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,12 @@ var wrongAnswers = 1;
 var correctPercent = correctAnswers/wrongAnswers ;
 // Use this to track if the game was completed early or not
 var finishClicked = false;
+// timer
+var timeOut;
+// counter variables
+var minutes;
+var seconds;
+var counter = 75;
 
 // array to hold boolean terms if the answers were correct or not
 var answerHold = [];
@@ -25,7 +31,9 @@ var h2Welcome = $("#h2Welcome");
 
 // Get things started
 function setUp (){
+	window.scrollTo(0,0);
 	// Hide the things we want hidden at the start...
+
 quizResults.hide();
 quiz.hide();
 textRunning.hide();
@@ -39,6 +47,26 @@ netCarbs.show();
 resetValues();
 console.log("The setUp function has run")
 };
+
+// Timer
+function convertSeconds(variable) {
+	 minutes = Math.floor(variable / 60);
+	 seconds = variable % 60;
+	
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	
+	if (minutes === 0) {
+		minutes = "00";
+	}
+	else if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	
+	return minutes + ":" + seconds;
+	};
+
 
 // set values to the original values
 function resetValues(){
@@ -60,20 +88,20 @@ timer.show();
 textWelcome.hide();
 h2Welcome.hide();
 netCarbs.hide();
-// start the timer
 
-// code code code code
-// code code code code
-// code code code code
-// code code code code
-// code code code code
-// code code code code
-// code code code code
 console.log("The quiz has started!")
+// Start a time limit of 45 seconds that will push quizEnd after
+// It's at three seconds for testing tho b/c ain't nobody got time for dat.  I have bronchitis etc etc
+timeOut = setTimeout(quizEnd, 3000);
+// Update the clock every second for 45 seconds
+setInterval(clockUpdate, 1000);
+
 };
 
 // Function that shows the results of the quiz, adds up the right and wrong elements and shows everything.
 function quizEnd(){
+		// clear out the timeOut variable
+		clearTimeout(timeOut)
 	// Show results and textFinished 
 	quizResults.show();
 	textFinished.show();
@@ -114,7 +142,5 @@ quizEnd();
 $("#resetBtn").on("click", function(){
 setUp();
 });
-
-
 // End function tracker
 });
